40ed0771bc3cf3548a30c1b62404ddf4d196e914
dana iser <diser@usatechsolutions.com>
2017-07-28 14:49:42 -0400

    Add files via upload

---
+++ msh-plug-in-tempalte.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: MSH-- Remove the additional information and Reviews Tab 
+ * Description: MSH--  Remove the additional information and Reviews Tab   
+ * Author: Dana Iser
+ * Version: 1.0
+ * Text Domain: MSH-- Woo Custom Remove the additional information and Reviews Tab
+ *
+ * Copyright: (c) 2012-2099 [Dana Iser] (diser@mainstreethost.com)
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html 
+ *
+ * @author    [MSH-Iser]
+ * @copyright Copyright (c) 2012-2014, [MSH-Iser]
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ *
+ */
+
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// drop your fubction here, save the file in a folder, and name both something that makes sense. Drop thisfile and its folder into the plugins folder and activate
+
